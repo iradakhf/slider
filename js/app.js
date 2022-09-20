6a77db15b3edc64a26ef3d098c12a62e57e2ef17
iradakhf <iradakhf@code.edu.az>
2022-09-20 10:11:20 +0400

    added all

---
+++ js/app.js
@@ -0,0 +1,53 @@
+let slider = [
+  "images/hero-berlin.jpg",
+   "images/img-2019_07_11_12_11_09-7279-crop577.jpg", 
+   "images/p00vsc8y.jpg"
+  ];
+
+let image = document.querySelector(".slider img");
+image.setAttribute("src", slider[0]);
+let index= 0;
+let next = document.querySelector(".next");
+next.addEventListener("click", function () {
+     index++;
+     if(index> slider.length-1){
+      index=0;
+     }
+     image.src= slider[index];
+});
+
+function AutoPlay(){
+  index++;
+     if(index> slider.length-1){
+      index=0;
+     }
+     image.src= slider[index];
+}
+let previous = document.querySelector(".previous");
+
+previous.addEventListener("click", function() {
+   index--;
+   if(index<0){
+    index = slider.length-1;
+   }
+   image.src = slider[index];
+
+})
+setInterval(() => {
+   AutoPlay();
+}, 3000);
+
+let boxes = document.querySelectorAll(".generalDots .box");
+for (let i = 0; i < boxes.length; i++) {
+    index.onchange = function() {
+    let active = document.querySelector(".active");
+     if(index==i){
+      boxes[i].classList.add("active");
+     }
+     else{
+      boxes[i].classList.remove("active");
+     }
+    }
+    
+}
+
